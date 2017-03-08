630f2b374caa4c1e7c0d6af70016d26f492d1809
Alexei Barantsev <barancev@gmail.com>
2017-03-08 13:06:18 +0300

    Javadoc

--- src/main/java/ru/stqa/selenium/factory/WebDriverPool.java
+++ src/main/java/ru/stqa/selenium/factory/WebDriverPool.java
@@ -22,30 +22,61 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
+ * An utility that helps to create, reuse and dismiss WebDriver instances.
+ *
+ * The WebDriver instances created by the pool and stored in the pool are called "managed instances".
+ *
+ * There are three ways to manage instances, three classes that implement WebDriverPool interface:
+ * <ul>
+ * <li>{@link SingleWebDriverPool} allows a single managed instance of WebDriver to exist at any given moment,</li>
+ * <li>{@link ThreadLocalSingleWebDriverPool} allows a single managed instance of WebDriver to exist for each thread,</li>
+ * <li>{@link LooseWebDriverPool} does not impose any restrictions, it creates a new managed instance on each request</li>
+ * </ul>
+ *
  * See documentation at https://github.com/barancev/webdriver-factory/
+ * See usage examples at https://github.com/barancev/webdriver-factory-samples/
  */
 public interface WebDriverPool {
 
+  /**
+   * Default pool
+   */
   WebDriverPool DEFAULT = new ThreadLocalSingleWebDriverPool();
 
+  /**
+   * Returns a managed local instance of WebDriver for the given browser.
+   * @param browser The desired browser
+   */
   default WebDriver getDriver(String browser) {
     return getDriver((URL) null, browser);
   }
 
+  /**
+   * Returns a managed remote instance of WebDriver for the given browser.
+   * @param hub The Selenium Server Hub address
+   * @param browser The desired browser
+   */
   default WebDriver getDriver(URL hub, String browser) {
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setBrowserName(browser);
     return getDriver(capabilities);
   }
 
+  /**
+   * Returns a managed local instance of WebDriver with the given capabilities.
+   * @param capabilities The desired driver capabilities
+   */
   default WebDriver getDriver(Capabilities capabilities) {
     return getDriver((URL) null, capabilities);
   }
 
+  /**
+   * Returns a managed remote instance of WebDriver with the given capabilities.
+   * @param hub The Selenium Server Hub address
+   * @param capabilities The desired driver capabilities
+   */
   WebDriver getDriver(URL hub, Capabilities capabilities);
 
   @Deprecated
@@ -57,10 +88,21 @@ public interface WebDriverPool {
     }
   }
 
+  /**
+   * Quits the driver and removes it from the pool, if it is a managed instance.
+   * Throws an Error on attempt to dismiss an unmanaged instance of WebDriver.
+   * @param driver The driver that is not in use anymore and should be dismissed
+   */
   void dismissDriver(WebDriver driver);
 
+  /**
+   * Quits all the managed drivers and empties the pool.
+   */
   void dismissAll();
 
+  /**
+   * Checks if the pool is empty.
+   */
   boolean isEmpty();
 
   void setDriverAlivenessChecker(DriverAlivenessChecker checker);
